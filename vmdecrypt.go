e897b985875595dc661fb5359fabe1afcbd12cd2
Radoslav Gerganov <rgerganov@gmail.com>
2021-01-28 12:44:16 +0200

    One channel support

--- vmdecrypt.go
+++ vmdecrypt.go
@@ -13,37 +13,55 @@ import (
 	"sync"
 )
 
-var lastRTPSeq uint16 = 0
-var firstPkt = true
-var pmtPid uint16
-var pmtPidFound = false
-var ecmPid uint16
-var ecmPidFound = false
-var masterKey = "09ce6d566f42302278a4b8121d6a4c74"
-var aes_key_1 []byte
-var aes_key_2 []byte
+type Channel struct {
+	lastRTPSeq  uint16
+	firstPkt    bool
+	pmtPid      uint16
+	pmtPidFound bool
+	ecmPid      uint16
+	ecmPidFound bool
+	masterKey   string
+	aes_key_1   []byte
+	aes_key_2   []byte
+	mu          sync.Mutex
+	buf         *ring.Ring
+	c           *sync.Cond
+	done        chan bool
+}
 
-var mu sync.Mutex
-var buf = ring.New(50)
-var c = sync.NewCond(&mu)
+type ChannelInfo struct {
+	ch         *Channel
+	numClients int
+}
 
-func parseRTP(pkt []byte) []byte {
+const RingSize = 64
+
+var channelMapMu sync.Mutex
+var channelMap map[string]*ChannelInfo
+
+func newChannel(masterKey string) *Channel {
+	ch := Channel{firstPkt: true, masterKey: masterKey}
+	ch.buf = ring.New(RingSize)
+	ch.c = sync.NewCond(&ch.mu)
+	ch.done = make(chan bool)
+	return &ch
+}
+
+func (ch *Channel) parseRTP(pkt []byte) []byte {
 	version := pkt[0] >> 6
 	if version != 2 {
 		log.Fatal("Unexpected RTP version ", version)
 	}
 	hasExtension := (pkt[0] >> 4) & 1
 	seq := binary.BigEndian.Uint16(pkt[2:4])
-	//ts := binary.BigEndian.Uint32(pkt[4:8])
-	//log.Println(seq)
-	if firstPkt {
-		lastRTPSeq = seq - 1
-		firstPkt = false
+	if ch.firstPkt {
+		ch.lastRTPSeq = seq - 1
+		ch.firstPkt = false
 	}
-	if lastRTPSeq+1 != seq {
+	if ch.lastRTPSeq+1 != seq {
 		log.Fatal("RTP discontinuity detected")
 	}
-	lastRTPSeq = seq
+	ch.lastRTPSeq = seq
 	extSize := 0
 	if hasExtension > 0 {
 		extSize = 4 + int(binary.BigEndian.Uint16(pkt[14:16])*4)
@@ -51,12 +69,11 @@ func parseRTP(pkt []byte) []byte {
 	return pkt[12+extSize:]
 }
 
-func processECM(pkt []byte) {
-	key, err := hex.DecodeString(masterKey)
+func (ch *Channel) processECM(pkt []byte) {
+	key, err := hex.DecodeString(ch.masterKey)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//log.Printf("% x", key)
 	cipher, _ := aes.NewCipher([]byte(key))
 	ecm := make([]byte, 64)
 	for i := 0; i < 4; i++ {
@@ -66,16 +83,16 @@ func processECM(pkt []byte) {
 		log.Fatal("Error decrypting ECM")
 	}
 	if pkt[5] == 0x81 {
-		aes_key_1 = ecm[9 : 9+16]
-		aes_key_2 = ecm[25 : 25+16]
+		ch.aes_key_1 = ecm[9 : 9+16]
+		ch.aes_key_2 = ecm[25 : 25+16]
 	} else {
-		aes_key_2 = ecm[9 : 9+16]
-		aes_key_1 = ecm[25 : 25+16]
+		ch.aes_key_2 = ecm[9 : 9+16]
+		ch.aes_key_1 = ecm[25 : 25+16]
 	}
 }
 
-func decodePacket(pkt []byte) {
-	if aes_key_1 == nil || aes_key_2 == nil {
+func (ch *Channel) decodePacket(pkt []byte) {
+	if ch.aes_key_1 == nil || ch.aes_key_2 == nil {
 		return
 	}
 	scramble := (pkt[3] >> 6) & 3
@@ -84,9 +101,9 @@ func decodePacket(pkt []byte) {
 	}
 	var aes_key []byte
 	if scramble == 2 {
-		aes_key = aes_key_2
+		aes_key = ch.aes_key_2
 	} else if scramble == 3 {
-		aes_key = aes_key_1
+		aes_key = ch.aes_key_1
 	}
 	cipher, _ := aes.NewCipher([]byte(aes_key))
 	pkt = pkt[4:]
@@ -107,12 +124,12 @@ func savePacket(pkt []byte) {
 	}
 }
 
-func processPacket(pkt []byte) {
+func (ch *Channel) processPacket(pkt []byte) {
 	if pkt[0] != 0x47 {
 		log.Fatal("Expected sync byte")
 	}
 	pid := binary.BigEndian.Uint16(pkt[1:3]) & 0x1fff
-	if !pmtPidFound && pid == 0 {
+	if !ch.pmtPidFound && pid == 0 {
 		// process PAT
 		if pkt[4] != 0 {
 			log.Fatal("Pointer fields are not supported yet")
@@ -120,11 +137,11 @@ func processPacket(pkt []byte) {
 		if pkt[5] != 0 {
 			log.Fatal("Unexpected PAT table ID", pkt[5])
 		}
-		pmtPid = binary.BigEndian.Uint16(pkt[15:17]) & 0x1fff
-		pmtPidFound = true
-		log.Printf("PMT pid=0x%x", pmtPid)
+		ch.pmtPid = binary.BigEndian.Uint16(pkt[15:17]) & 0x1fff
+		ch.pmtPidFound = true
+		log.Printf("PMT pid=0x%x", ch.pmtPid)
 	}
-	if !ecmPidFound && pmtPidFound && pid == pmtPid {
+	if !ch.ecmPidFound && ch.pmtPidFound && pid == ch.pmtPid {
 		// process PMT
 		if pkt[4] != 0 {
 			log.Fatal("Pointer fields are not supported yet")
@@ -139,35 +156,50 @@ func processPacket(pkt []byte) {
 		if caid != 0x5601 {
 			log.Fatal("Unexpected CAID", caid)
 		}
-		ecmPid = binary.BigEndian.Uint16(pkt[21:23])
-		ecmPidFound = true
-		log.Printf("ECM pid=0x%x", ecmPid)
+		ch.ecmPid = binary.BigEndian.Uint16(pkt[21:23])
+		ch.ecmPidFound = true
+		log.Printf("ECM pid=0x%x", ch.ecmPid)
 	}
-	if ecmPidFound && pid == ecmPid {
-		processECM(pkt)
+	if ch.ecmPidFound && pid == ch.ecmPid {
+		ch.processECM(pkt)
 	}
-	decodePacket(pkt)
+	ch.decodePacket(pkt)
 
-	mu.Lock()
-	buf.Value = pkt
-	buf = buf.Next()
-	c.Broadcast()
-	mu.Unlock()
+	ch.mu.Lock()
+	ch.buf.Value = pkt
+	ch.buf = ch.buf.Next()
+	ch.c.Broadcast()
+	ch.mu.Unlock()
 	//savePacket(pkt)
 	//log.Printf("% x\n", pkt)
 }
 
-func parsePayload(pkt []byte) {
+func (ch *Channel) parsePayload(pkt []byte) {
 	if len(pkt)%188 != 0 {
 		log.Fatal("Unexpected length")
 	}
 	for len(pkt) > 0 {
-		processPacket(pkt[:188])
+		ch.processPacket(pkt[:188])
 		pkt = pkt[188:]
 	}
 }
 
-func vmdecrypt() {
+func (ch *Channel) currentPtr() *ring.Ring {
+	ch.mu.Lock()
+	defer ch.mu.Unlock()
+	return ch.buf
+}
+
+func (ch *Channel) nextPtr(ptr *ring.Ring) (*ring.Ring, interface{}) {
+	ch.mu.Lock()
+	defer ch.mu.Unlock()
+	for ptr == ch.buf {
+		ch.c.Wait()
+	}
+	return ptr.Next(), ptr.Value
+}
+
+func vmdecrypt(ch *Channel) {
 	eth1, err := net.InterfaceByName("eth1")
 	if err != nil {
 		log.Fatal(err)
@@ -185,39 +217,71 @@ func vmdecrypt() {
 		log.Fatal(err)
 	}
 
+	log.Println("Start decrypting channel ...")
+loop:
 	for {
+		select {
+		case <-ch.done:
+			break loop
+		default:
+			// do nothing
+		}
 		pkt := make([]byte, 1500)
 		n, _, _, err := p.ReadFrom(pkt)
 		if err != nil {
 			log.Fatal(err)
 		}
-		payload := parseRTP(pkt[:n])
-		parsePayload(payload)
+		payload := ch.parseRTP(pkt[:n])
+		ch.parsePayload(payload)
 	}
+	log.Println("Stopped decrypting channel")
+	p.LeaveGroup(eth1, &net.UDPAddr{IP: group})
+	ch.done <- true
 	//log.Println(n, "bytes read from", src)
 	//log.Printf("% x", b[:n])
 }
 
 func httpHandler(w http.ResponseWriter, req *http.Request) {
-	mu.Lock()
-	ptr := buf
-	mu.Unlock()
-
-	log.Println("Serving client ...")
-	for {
-		mu.Lock()
-		for ptr == buf {
-			c.Wait()
-		}
-		val := ptr.Value
-		ptr = ptr.Next()
-		mu.Unlock()
-		w.Write(val.([]byte))
+	channelMapMu.Lock()
+	chInfo, ok := channelMap["btv"]
+	if !ok {
+		ch := newChannel("48a028403963ae6bb8a26ec85677567e")
+		chInfo = &ChannelInfo{ch: ch, numClients: 1}
+		channelMap["btv"] = chInfo
+		go vmdecrypt(ch)
+	} else {
+		chInfo.numClients += 1
 	}
+	channelMapMu.Unlock()
+
+	ch := chInfo.ch
+	log.Println("Start serving client")
+	ptr := ch.currentPtr()
+	var val interface{}
+	for {
+		ptr, val = ch.nextPtr(ptr)
+		_, err := w.Write(val.([]byte))
+		if err != nil {
+			break
+		}
+	}
+
+	log.Println("Stop serving client")
+	channelMapMu.Lock()
+	chInfo, ok = channelMap["btv"]
+	if ok {
+		chInfo.numClients -= 1
+		if chInfo.numClients == 0 {
+			chInfo.ch.done <- true
+			<-chInfo.ch.done
+			delete(channelMap, "btv")
+		}
+	}
+	channelMapMu.Unlock()
 }
 
 func main() {
-	go vmdecrypt()
+	channelMap = make(map[string]*ChannelInfo)
 	http.HandleFunc("/", httpHandler)
 	log.Fatal(http.ListenAndServe(":8090", nil))
 }
